e96919d054fe72202830edce9289cca3cebe311b
Vishwanath Sangale <vishsangale@gmail.com>
2016-11-02 16:09:18 -0700

    Tutorial 1 - Calculating power of 2.

--- tutorial1.c
+++ tutorial1.c
@@ -1,6 +1,30 @@
-#include <iostream>
+/*
+ * Tutorial to calculate power of 2.
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float calculate_power(int exponent) {
+    if (exponent == 0) {
+        return 0;
+    }
+    int i;
+    float val = 1;
+    for (i = 0; i < abs(exponent); ++i) {
+        val *= 2;
+    }
+    if (exponent < 0) {
+        val = 1/val;
+    }
+    return val;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int exponent;
+    printf("Enter exponent to calculate power of 2\n");
+    scanf("%d", &exponent);
+    float power = calculate_power(exponent);
+    printf("Calculated power of 2 = %f\n", power);
     return 0;
 }
